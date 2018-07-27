3246d9d9ca2f029e5fe3e4ca26329e2fa9119259
github username <github email address>
2018-07-26 22:15:53 -0400

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,37 @@
+class Ingredient
+    
+    @@all = []
 
+    def initialize
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def allergens
+        Allergen.all.select do |allergen|
+            allergen.ingredient == self
+        end
+    end
+
+    def users
+        allergens.map do |allergen|
+            allergen.user
+        end 
+    end
+
+    def self.most_common_allergen
+        # self.all.sort_by do |allergen|
+        #     allergen.users.length
+        # end[-1]
+        i = Allergen.all.map do |allergen|
+            allergen.ingredient
+        end
+        i.sort_by do |el|
+            i.count(el)
+        end[-1]
+        
+    end
+end
---
+++ app/models/User.rb
@@ -0,0 +1,61 @@
+class User
+    
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_recipe_card(recipe, date, rating)
+        RecipeCard.new(user: self, rating: rating, date: date, recipe: recipe)
+    end
+
+    def my_recipe_cards
+        RecipeCard.all.select do |recipe_card|
+            recipe_card.user == self
+        end
+    end
+
+    def recipes
+        my_recipe_cards.map do |recipe_card|
+            recipe_card.recipe
+        end
+    end
+
+    def top_three_recipes
+        my_recipe_cards.sort_by do |recipe_card|
+            recipe_card.rating
+        end.map do |recipe_card|
+            recipe_card.recipe
+        end[-3..-1]
+    end
+
+    def most_recent_recipe
+        self.recipes[-1]
+    end
+
+    def declare_allergen(ingredient)
+        Allergen.new(self, ingredient)
+    end
+
+    def allergens
+        Allergen.all.select do |allergen|
+            allergen.user == self
+        end.map do |allergen|
+            allergen.ingredient
+        end
+    end
+
+    def safe_recipes
+        recipes.select do |recipe|
+            recipe.allergens.all? do |allergen|
+                !self.allergens.include?(allergen)
+            end
+        end
+    end
+
+end
